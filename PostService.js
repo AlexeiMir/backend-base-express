78013800c53f0b5d74d8e4017121f1991d623c8d
AlexeiMir <alex_16.07@mail.ru>
2022-02-12 14:36:40 +0700

    Express course

---
+++ PostService.js
@@ -0,0 +1,39 @@
+import fileService from './fileService.js'
+import Post from "./Post.js";
+
+class PostService {
+    async create(post, picture){
+        const fileName = fileService.saveFile(picture);
+        const createdPost = await Post.create({...post, picture: fileName})
+        return createdPost;
+    }
+    async getAll(){
+        const posts = await Post.find();
+        return posts;
+    }
+    async getOne(id){
+        if (!id){
+            throw new Error('не указан ID');
+        }
+        const post = Post.findById(id);
+        return post;
+    }
+    async update(post){
+        if (!post._id){
+            throw new Error('не указан ID');
+        }
+        const updatedPost = Post.findByIdAndUpdate(post._id, post,{new: true} );
+        return updatedPost;
+    }
+
+    async delete(id){
+        if (!id){
+            throw new Error('не указан ID');
+        }
+        const post = Post.findByIdAndDelete(id);
+        return post;
+    }
+
+}
+
+export default new PostService();
